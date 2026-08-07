43e18bb7ca20ad7ccf646d5e8cd598a581df476e
Seigo.k <216270240+seigo-gace@users.noreply.github.com>
2026-08-07 11:35:01 +0900

    chore: remove redundant script entry [skip ci]

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-import { initLanguageSelect } from './language.js';
-import { initSideMenu } from './menu.js';
-import { initAiBubble } from './ai-bubble.js';
-import { initAppEntry } from './app-entry.js';
-
-initLanguageSelect();
-initSideMenu();
-initAiBubble();
-initAppEntry();
