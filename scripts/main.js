22f490d7cc06c5bb09d51b0f04cb2d91a97472dc
Seigo.k <216270240+seigo-gace@users.noreply.github.com>
2026-08-07 11:28:53 +0900

    refactor: add static site entry script [skip ci]

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+import { initLanguageSelect } from './language.js';
+import { initSideMenu } from './menu.js';
+import { initAiBubble } from './ai-bubble.js';
+
+initLanguageSelect();
+initSideMenu();
+initAiBubble();
