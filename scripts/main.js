48364afb11b5b9594cb2999c94eaa359e5d785a7
Seigo.k <216270240+seigo-gace@users.noreply.github.com>
2026-08-07 11:31:57 +0900

    refactor: compose isolated static modules [skip ci]

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import { initLanguageSelect } from './language.js';
 import { initSideMenu } from './menu.js';
 import { initAiBubble } from './ai-bubble.js';
+import { initAppEntry } from './app-entry.js';
 
 initLanguageSelect();
 initSideMenu();
 initAiBubble();
+initAppEntry();
