d1672559d1ad3e3a6efb4c4e7438f65ef560ad8a
Seigo.k <216270240+seigo-gace@users.noreply.github.com>
2026-08-08 17:42:05 +0900

    feat: register hero effect module

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ import { initLanguageSelect } from './scripts/language.js';
 import { initSideMenu } from './scripts/menu.js';
 import { initAiBubble } from './scripts/ai-bubble.js';
 import { initAppEntry } from './scripts/app-entry.js';
+import { initHeroEffect } from './scripts/hero-effect.js';
 
 initLanguageSelect();
 initSideMenu();
 initAiBubble();
 initAppEntry();
+initHeroEffect();
