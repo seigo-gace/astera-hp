a061693c7a38dc7ab43f51c242a2f289cf16b512
Seigo.k <216270240+seigo-gace@users.noreply.github.com>
2026-08-10 02:37:19 +0900

    feat: initialize Main10 interface

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ import { initSideMenu } from './scripts/menu.js';
 import { initAiBubble } from './scripts/ai-bubble.js';
 import { initAppEntry } from './scripts/app-entry.js';
 import { initHeroEffect } from './scripts/hero-effect.js';
+import { initMain10 } from './scripts/main10.js';
 
 initLanguageSelect();
 initSideMenu();
 initAiBubble();
 initAppEntry();
 initHeroEffect();
+initMain10();
