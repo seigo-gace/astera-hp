29f44e214e721e7b001be35d56084275b2280031
Seigo.k <216270240+seigo-gace@users.noreply.github.com>
2026-08-10 01:18:45 +0900

    refactor: remove current Main 10 cards

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ import { initSideMenu } from './scripts/menu.js';
 import { initAiBubble } from './scripts/ai-bubble.js';
 import { initAppEntry } from './scripts/app-entry.js';
 import { initHeroEffect } from './scripts/hero-effect.js';
-import { initMain10Space } from './scripts/main10-space.js?v=cu23-floating-space';
 
 initLanguageSelect();
 initSideMenu();
 initAiBubble();
 initAppEntry();
 initHeroEffect();
-initMain10Space();
